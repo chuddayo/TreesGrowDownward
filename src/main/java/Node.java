e2c9fc8637512f96b86af32394653a26ac88c169
Ryan Chudd <ryan.a.chudd@gmail.com>
2022-08-05 20:44:04 -0400

    node POJO getters and setters

--- src/main/java/Node.java
+++ src/main/java/Node.java
@@ -11,4 +11,30 @@ public class Node {
         right = null;
         left = null;
     }
+
+    Node() {}
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
 }
